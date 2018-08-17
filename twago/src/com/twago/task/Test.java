16cdf0643deaf72d1fc7c2a8d14fab0929945562
moatazmotawea <42440528+moatazmotawea@users.noreply.github.com>
2018-08-17 15:18:14 +0200

    BE Coding

--- twago/src/com/twago/task/Test.java
+++ twago/src/com/twago/task/Test.java
@@ -9,57 +9,68 @@ import java.util.Scanner;
  */
 public class Test {
 
-	static int solution(int x, int y) {
+	static int solution(Scanner scanner, int x, int y) {
 
-		Scanner scanner = new Scanner(System.in);
 		int i, j, k;
 		int[] numList = new int[x];
 
 		try {
-				while (y > 0) {
-	
-					// read input ex: 1 2 100
-					String[] operation = scanner.nextLine().split(" ");
-	
-					i = Integer.parseInt(operation[0]) - 1;
-					j = Integer.parseInt(operation[1]) - 1;
-					k = Integer.parseInt(operation[2]);
-	
-					// add k from i to j
-					for (int l = i; l <= j; l++) {
-						numList[l] += k;
-					}
-	
-					// operations counter
-					y--;
-	
-				}
-	
-				// ascending sorting to get the max
-				Arrays.sort(numList);
-	
-				return numList[x - 1];
+			while (y > 0) {
 
-		} catch (NumberFormatException | IndexOutOfBoundsException e ) {
-			System.out.println("List size and opertions must be integers \n Input format must be integers like these: 1 2 100");
+				// read input ex: 1 2 100
+				String[] operation = scanner.nextLine().split(" ");
+
+				i = Integer.parseInt(operation[0]) - 1;
+				j = Integer.parseInt(operation[1]) - 1;
+				k = Integer.parseInt(operation[2]);
+
+				// add k from i to j
+				for (int l = i; l <= j; l++) {
+					numList[l] += k;
+				}
+
+				// operations counter
+				y--;
+
+			}
+
+			// ascending sorting to get the max
+			Arrays.sort(numList);
+
+			return numList[x - 1];
+
+		} catch (NumberFormatException e) {
+			System.out.println("Input format must be integers like these:1 2 100");
 			return -1;
-		}
-		catch (Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 			return -1;
-		}
-		finally {
-			scanner.close();
 		}
 
 	}
 
 	public static void main(String[] args) {
 
-		int x = 5;
-		int y = 3;
+		int x, y;
+		Scanner scanner = new Scanner(System.in);
 
-		System.out.println(solution(x, y));
+		try {
+
+			System.out.println("Enter array size and number of operations: ");
+			String[] inputs = scanner.nextLine().split(" ");
+
+			x = Integer.parseInt(inputs[0]);
+			y = Integer.parseInt(inputs[1]);
+
+			System.out.println(solution(scanner, x, y));
+
+		} catch (NumberFormatException | IndexOutOfBoundsException e) {
+			System.out.println("List size and opertions must be integers like these:5 3");
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			scanner.close();
+		}
 
 	}
 
